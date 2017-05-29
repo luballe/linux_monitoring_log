474c02473cda77c366160dfd8e974fc697fba3dc
Luis Ballesteros <luballe@luballe.com>
2017-05-29 12:57:00 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# Installing the best terminal multiplexer and the best interactive monitoring tool (just to make sure the data logs are correct)
+apt-get install -y htop tmux
+# Installing pip and python development kit
+apt-get install -y python-pip python-dev
+# Installing system stats tool
+apt-get install -y sysstat
+# Installing git client
+apt-get install -y git
+git clone https://github.com/luballe/monitoring.git
